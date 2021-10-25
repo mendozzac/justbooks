80e37a6e521ac1594e06f5ea3758d90393839a66
Fernando Mendoza <87226298+mendozzac@users.noreply.github.com>
2021-10-25 21:10:21 +0200

    add BEM names in BookCard component

--- src/components/BookCard/BookCard.js
+++ src/components/BookCard/BookCard.js
@@ -2,16 +2,16 @@ const BookCard = () => {
   return (
     <div className="card">
       <img
-        className="card-img-top"
+        className="card__image"
         src="http://books.google.com/books/content?id=PP6SDQAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api"
         alt="loacting the voice in the film"
         width="20"
         height="70"
       ></img>
-      <div className="card-body">
-        <h3 className="card-title">Title of book</h3>
-        <p className="card-author"> Author </p>
-        <p className="price"> Price </p>
+      <div className="card__content">
+        <h3 className="card__title">Title of book</h3>
+        <p className="card__author"> Author </p>
+        <p className="card__price"> Price </p>
       </div>
     </div>
   );
