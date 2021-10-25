c7c1cc6ea6019aeac8086bf91d8f00727ab994fa
Fernando Mendoza <87226298+mendozzac@users.noreply.github.com>
2021-10-25 14:18:07 +0200

    add basic structure jsx App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Router>
+        <header className="header">
+          <div className="container header-container"></div>
+        </header>
+        <main className="main-content">
+          <div className="container main-container"></div>
+        </main>
+        <footer className="footer">
+          <div className="container footer-container"></div>
+        </footer>
+      </Router>
+    </>
+  );
 }
 
 export default App;
