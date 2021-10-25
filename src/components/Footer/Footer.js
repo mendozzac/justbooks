eae05bbb6165ff5243e7081bc5463fac3d27185d
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-10-25 13:57:32 +0200

    Added Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+const Footer =()=> {
+  return (
+    <footer class="page-footer font-small brown">
+        <div class="text-center"> 
+          <p> Terms & Conditions </p>
+        </div>  
+    </footer>
+  )
+}
+
+export default Footer;
