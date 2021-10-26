12723493576b29ac7e8ba5514ee98f83215c97a7
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-10-26 12:40:09 +0200

    Added custom hooks

--- src/hooks/useBooks.js
+++ src/hooks/useBooks.js
@@ -1,18 +1,22 @@
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
+import { loadBooksAction } from "../store/actions/actionCreators";
 import BooksContext from "../store/contexts/BooksContext";
 
 const useBooks = () => {
-  let { books } = useContext(BooksContext);
-  const loadBooks = async () => {
+  let { books, dispatch } = useContext(BooksContext);
+
+  const loadBooks = useCallback(async () => {
     const bookUrl =
       "https://www.googleapis.com/books/v1/volumes?q=subject:art&printType=books&filter=paid-ebooks&startIndex=0&maxResults=10";
     const response = await fetch(bookUrl);
     const books = await response.json();
-  };
+    dispatch(loadBooksAction(books));
+  }, [dispatch]
+  );
 
   return {
     books,
-    loadBooks,
+    loadBooks
   };
 };
 
