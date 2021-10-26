9b55f55ff88fa3f8b03d377f96f77220871f80b5
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-10-26 10:47:00 +0200

    Created actionCreators

---
+++ src/store/actions/actionCreators.js
@@ -0,0 +1,18 @@
+import actionTypes from "./actionTypes"
+
+export const  loadBooksAction = (books) => ({
+  type: actionTypes.loadBooks,
+  books
+});
+
+export const deleteBookAction = (id) => ({
+  type: actionTypes.deleteBook,
+  id
+})
+
+export const createBook = (book) => ({
+  type: actionTypes.createBook,
+  book
+})
+
+
