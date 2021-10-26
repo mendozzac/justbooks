a0bb6e0dcc4f6b95ff776a3f513c144f080f2075
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-10-26 17:05:15 +0200

    Added test bookreducer

---
+++ src/factories/bookFactory.js
@@ -0,0 +1,33 @@
+const faker = require('faker');
+const fs = require('fs')
+
+const generateBooks = () => {
+
+  let books = []
+
+  for (let id=1; id <= 10; id++) {
+
+    let image = faker.image.image();
+    let title = faker.name.title();
+    let author = faker.name.firstName() + faker.name.lastName();
+    let price = faker.commerce.price();
+
+    books.push({
+        // "id": id,
+        "image": image,
+        "title": title,
+        "author": author,
+        "price": price,
+    });
+  }
+
+  return { "data": books }
+}
+
+let dataObj = generateBooks();
+
+fs.writeFileSync('data.json', JSON.stringify(dataObj, null, '\t'));
+
+//console.log(dataObj.data);
+
+export default generateBooks;
