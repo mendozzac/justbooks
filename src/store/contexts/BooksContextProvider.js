421c3191db82728ed5647b0f48b71ac162742917
Menchu MV <mcmvillalobos@gmail.com>
2021-10-27 20:50:43 +0200

    Added reset function to logo

--- src/store/contexts/BooksContextProvider.js
+++ src/store/contexts/BooksContextProvider.js
@@ -1,15 +1,28 @@
 import BooksContext from "./BooksContext";
-import { useReducer, useState } from "react";
+import { useReducer, useRef, useState } from "react";
 import booksReducer from "../reducers/bookReducer/booksReducer";
 
 const BooksContextProvider = ({ children }) => {
   const [books, dispatch] = useReducer(booksReducer, []);
   const [page, setPage] = useState(0);
+  const query = useRef("");
+  const [title, setTitle] = useState("Our Books");
 
   const [startIndex, setStartIndex] = useState(0);
   const resetIndex = () => {
     setStartIndex(0);
   };
+
+  const resetQuery = () => {
+    query.current = "";
+  };
+
+  const resetHomepage = () => {
+    resetIndex();
+    resetQuery();
+    setTitle("Our Books");
+  };
+
   return (
     <BooksContext.Provider
       value={{
@@ -20,6 +33,11 @@ const BooksContextProvider = ({ children }) => {
         startIndex,
         setStartIndex,
         resetIndex,
+        query,
+        resetQuery,
+        resetHomepage,
+        title,
+        setTitle,
       }}
     >
       {children}
