0139e1c64e45078fafdf9b4a928ae0d6ef3c01a1
Menchu MV <mcmvillalobos@gmail.com>
2021-10-27 09:35:58 +0200

    Added list group class to categories

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -21,24 +21,24 @@ const SideBar = () => {
             <div className="mybooks-link">Categories</div>
           </li>
           <li className="dropdown-item">
-            <ul className="sidebar__categories">
-              <li>Art</li>
-              <li>Biography</li>
-              <li>Children's Books</li>
-              <li>Crafts & Hobbies</li>
-              <li>Crime & Thriller</li>
-              <li>Dictionaries</li>
-              <li>Fiction</li>
-              <li>Graphic Novels</li>
-              <li>History</li>
-              <li>Spirituality</li>
-              <li>Poetry</li>
-              <li>Romance</li>
-              <li>Science</li>
-              <li>Sport</li>
-              <li>Technology</li>
-              <li>Teen & Young Adult</li>
-              <li>Travel</li>
+            <ul className="sidebar__categories list-group">
+              <li className="list-group-item">Art</li>
+              <li className="list-group-item">Biography</li>
+              <li className="list-group-item">Children's Books</li>
+              <li className="list-group-item">Crafts & Hobbies</li>
+              <li className="list-group-item">Crime & Thriller</li>
+              <li className="list-group-item">Dictionaries</li>
+              <li className="list-group-item">Fiction</li>
+              <li className="list-group-item">Graphic Novels</li>
+              <li className="list-group-item">History</li>
+              <li className="list-group-item">Spirituality</li>
+              <li className="list-group-item">Poetry</li>
+              <li className="list-group-item">Romance</li>
+              <li className="list-group-item">Science</li>
+              <li className="list-group-item">Sport</li>
+              <li className="list-group-item">Technology</li>
+              <li className="list-group-item">Teen & Young Adult</li>
+              <li className="list-group-item">Travel</li>
             </ul>
           </li>
         </ul>
