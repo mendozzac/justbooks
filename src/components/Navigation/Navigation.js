c32d11c5e94208394f2e30a4ff66d8370014650f
Fernando Mendoza <87226298+mendozzac@users.noreply.github.com>
2021-10-28 08:47:21 +0200

    add pagination button styles in Navigation component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,10 +4,10 @@ const Navigation = () => {
       <nav aria-label="...">
         <ul className="pagination pagination-lg">
           <li className="page-item ">
-            <button className="btn btn-secondary carousel-control-prev-icon"></button>
+            <button className="pagination-button btn btn-secondary carousel-control-prev-icon"></button>
           </li>
           <li className="page-item">
-            <button className="btn btn-secondary carousel-control-next-icon"></button>
+            <button className="pagination-button btn btn-secondary carousel-control-next-icon"></button>
           </li>
         </ul>
       </nav>
