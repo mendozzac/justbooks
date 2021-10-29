1d6111e979d1fca0a45d4152f97712bc5cd5343a
Menchu MV <mcmvillalobos@gmail.com>
2021-10-29 10:31:34 +0200

    Added conditional rendering to mybooks

--- src/pages/MyBooks/MyBooks.js
+++ src/pages/MyBooks/MyBooks.js
@@ -3,6 +3,8 @@ import Button from "../../components/Button/Button";
 import { useEffect } from "react";
 import useBooks from "../../hooks/useBooks";
 import BookCard from "../../components/BookCard/BookCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 const MyBooks = () => {
   const { books, loadBooksApiLocal } = useBooks();
@@ -17,29 +19,38 @@ const MyBooks = () => {
     loadBooksApiLocal();
   }, [loadBooksApiLocal]);
 
-  return (
-    <>
-      <div className="header-subtitle container">
-        <h2>My Books</h2>
-        <NavLink to="/form" activeClassName="current-section" exact>
-          <Button text="+" className="btn-warning" />
-        </NavLink>
+  return books ? (
+    books.length ? (
+      <>
+        <div className="header-subtitle container">
+          <h2>My Books</h2>
+          <NavLink to="/form" activeClassName="current-section" exact>
+            <Button text="+" className="btn-warning" />
+          </NavLink>
+        </div>
+
+        <p> Currently you have {books.length} favourite books you can buy. </p>
+
+        <ul className="booklist">
+          {books.map((book) => (
+            <BookCard
+              key={book.id}
+              book={book}
+              actionOnClick={() => {}}
+              addToFav={() => {}}
+              removeFromFav={removeFromFav}
+            />
+          ))}
+        </ul>
+      </>
+    ) : (
+      <div className="container text-center mt-5">
+        <FontAwesomeIcon icon={faSpinner} spin />
+        <span className="sr-only">Loading...</span>
       </div>
-
-      <p> Currently you have {books.length} favourite books you can buy. </p>
-
-      <ul className="booklist">
-        {books.map((book) => (
-          <BookCard
-            key={book.id}
-            book={book}
-            actionOnClick={() => {}}
-            addToFav={() => {}}
-            removeFromFav={removeFromFav}
-          />
-        ))}
-      </ul>
-    </>
+    )
+  ) : (
+    <div className="text-center mt-5">No results</div>
   );
 };
 
